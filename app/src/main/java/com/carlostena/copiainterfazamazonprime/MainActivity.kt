1d3199503bcd5ce92b6afb5767bcfd58ebd91479
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-11-10 10:58:35 +0100

    Con listeners

--- app/src/main/java/com/carlostena/copiainterfazamazonprime/MainActivity.kt
+++ app/src/main/java/com/carlostena/copiainterfazamazonprime/MainActivity.kt
@@ -1,69 +1,51 @@
 package com.carlostena.copiainterfazamazonprime
 
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.HorizontalScrollView
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.carlostena.copiainterfazamazonprime.databinding.ActivityMainConstraintBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var imageViewPueblo : ImageView
-    lateinit var tvOriginals : TextView
-    lateinit var tvSeguir : TextView
-    lateinit var tvFavoritos : TextView
-    lateinit var hsvOriginals : HorizontalScrollView
-    lateinit var hsvSeguir : HorizontalScrollView
-    lateinit var hsvFavoritos : HorizontalScrollView
-
+    private lateinit var binding : ActivityMainConstraintBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_constraint)
+        binding = ActivityMainConstraintBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        imageViewPueblo = findViewById(R.id.iw_pueblo)
 
-        tvOriginals = findViewById(R.id.tv_origials)
-        tvSeguir = findViewById(R.id.tv_seguir_viendo)
-        tvFavoritos = findViewById(R.id.tv_favoritos)
-
-        hsvOriginals = findViewById(R.id.hsv_originals)
-        hsvSeguir = findViewById(R.id.hsv_seguir_viendo)
-        hsvFavoritos = findViewById(R.id.hsv_favoritos)
-
-        imageViewPueblo.setOnClickListener {
+        binding.iwPueblo.setOnClickListener {
             saludaAAmazon()
         }
 
-        hsvOriginals.visibility = View.GONE
-        hsvSeguir.visibility = View.GONE
-        hsvFavoritos.visibility = View.GONE
+        binding.hsvOriginals.visibility = View.GONE
+        binding.hsvSeguirViendo.visibility = View.GONE
+        binding.hsvFavoritos.visibility = View.GONE
 
-        tvOriginals.setOnClickListener {
-            if(hsvOriginals.visibility == View.VISIBLE)
-                hsvOriginals.visibility = View.GONE
+        binding.tvOrigials.setOnClickListener {
+            if(binding.hsvOriginals.visibility == View.VISIBLE)
+                binding.hsvOriginals.visibility = View.GONE
             else
-                hsvOriginals.visibility = View.VISIBLE
+                binding.hsvOriginals.visibility = View.VISIBLE
         }
-        tvSeguir.setOnClickListener {
-            if(hsvSeguir.visibility == View.VISIBLE)
-                hsvSeguir.visibility = View.GONE
+        binding.tvSeguirViendo.setOnClickListener {
+            if(binding.hsvSeguirViendo.visibility == View.VISIBLE)
+                binding.hsvSeguirViendo.visibility = View.GONE
             else
-                hsvSeguir.visibility = View.VISIBLE
+                binding.hsvSeguirViendo.visibility = View.VISIBLE
         }
-        tvFavoritos.setOnClickListener {
-            if(hsvFavoritos.visibility == View.VISIBLE)
-                hsvFavoritos.visibility = View.GONE
+        binding.tvFavoritos.setOnClickListener {
+            if(binding.hsvFavoritos.visibility == View.VISIBLE)
+                binding.hsvFavoritos.visibility = View.GONE
             else
-                hsvFavoritos.visibility = View.VISIBLE
+                binding.hsvFavoritos.visibility = View.VISIBLE
         }
 
     }
 
     fun saludaAAmazon(){
-        Toast.makeText(this, "Hola Amazon!", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Hola Amazon!" + binding.tvFavoritos.text, Toast.LENGTH_LONG).show()
     }
 }
